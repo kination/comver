cefb28dd1d3d2e7fdbf4f8abd6ccfa5822135d53
djKooks <inylove82@gmail.com>
2021-12-30 19:56:17 +0900

    Fix error

--- comver/main.py
+++ comver/main.py
@@ -15,19 +15,25 @@ def cli():
 @click.command()
 def server():
     # router = Router('')
-    async def __generate_response(value):
-        return value
+    async def __generate_response(request):
+        return JSONResponse({'a': 'b'})
 
+    '''
     route = {
         '/hello': JSONResponse({'hello' : 'world'}),
         '/world': JSONResponse({'world' : 'hello'}),
     }
+    
 
     generated_route = []
     for key, value in route.items():
         generated_route.append(Route(key, __generate_response(value)))
+    '''
 
-    app = Starlette(debug=True, routes=generated_route)
+    app = Starlette(debug=True, routes=[
+        Route('/hello', __generate_response),
+        Route('/world', __generate_response)
+    ])
 
     uvicorn.run(app, host="127.0.0.1", port=9000, log_level="info")
 
