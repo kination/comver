d56f2a277827938a4ff84369f39c079106a48f1a
djKooks <inylove82@gmail.com>
2022-03-21 08:54:01 +0900

    use separate dependencies for test

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 
 from comver import __app_name__, __version__
 
+
 here = os.path.dirname(os.path.abspath(__file__))
 
 with open('README.md') as f:
@@ -13,18 +14,21 @@ with open('README.md') as f:
 # with open('LICENSE') as f:
     # license = f.read()
 
-# Requirements
-with codecs.open(os.path.join(here, 'requirements.txt')) as f:
-    install_requirements = [
-        line.split('#')[0].strip() for line in f.readlines() if not line.startswith('#')
-    ]
+def get_requirements(file_name: str) -> str:
+    with codecs.open(os.path.join(here, file_name)) as f:
+        return [
+            line.split('#')[0].strip() for line in f.readlines() if not line.startswith('#')
+        ]   
 
 setup(
     name=__app_name__,
     version=__version__,
-    description='bla bla bla',
+    description='comver description',
     long_description=readme,
-    install_requires=install_requirements,
+    install_requires=get_requirements('requirements.txt'),
+    extras_require={
+        'test': get_requirements('test-requirements.txt')
+    },
     py_modules=['comver'],
     entry_points='''
         [console_scripts]
